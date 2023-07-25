773de5f740248cc346f0c7eae647dec53a214223
Faith Kamau <imanilee121@gmail.com>
2023-07-25 16:25:04 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character starting with the first
+ * @str: the string in questio
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
