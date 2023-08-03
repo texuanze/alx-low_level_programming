8618b25a9aa3e0957284f3573cd02e9380d05b88
Faith Kamau <imanilee121@gmail.com>
2023-08-03 03:37:45 -0700

    0-puts_recursion

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string in question
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Changes the null byte in a specified file and adds another one
+ * @dest: the destination file 
+ * @src: the source file 
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
